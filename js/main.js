8a70c67ec03f6794424ebcb903cff07a0765ca00
Gautam Mittal <gautam@mittal.net>
2019-05-20 16:02:18 -0700

    Add basic sprites and game loop.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,52 @@
-let type = "WebGL"
-if(!PIXI.utils.isWebGLSupported()){
-    type = "canvas"
+/**
+Boids simulation.
+Written by Gautam Mittal.
+**/
+
+/* Initialize new PIXI application */
+let app = new PIXI.Application({width: 512, height: 512})
+document.body.appendChild(app.view)
+
+/* Create sprite array */
+let boids = []
+
+let setup = () => {
+    for (var i = 0; i < 100; i += 1) {
+        let circle = new PIXI.Graphics();
+        circle.beginFill(0x9966FF)
+        circle.drawCircle(0, 0, 12)
+        circle.endFill()
+        circle.x = i*44
+        circle.y = 130
+        boids[i] = circle
+        app.stage.addChild(circle)
+    }
+
+    //{width: window.innerWidth, height: window.innerHeight})
+
+    /*
+      app.renderer.view.style.position = "absolute"
+      app.renderer.view.style.display = "block"
+      app.renderer.autoResize = true
+
+      window.addEventListener("resize", () => {
+      app.renderer.resize(window.innerWidth, window.innerHeight)
+      })
+    */
 }
 
-PIXI.utils.sayHello(type)
+let gameLoop = () => {
+    // Game drawing and updates go here
+    boids.forEach((boid) => {
+        boid.x += 10 * (Math.random() - 0.5);
+        boid.y += 10 * (Math.random() - 0.5);
+    })
+    requestAnimationFrame(gameLoop)
+}
 
-let app = new PIXI.Application({width: 256, height: 256});
+let play = (delta) => {
+    // Game logic goes here
+}
+
+setup()
+gameLoop()
